e9e4228769791bbec3e15b4864024ecabc4c3bae
Amber Zhou <zhou0160@algonquinlive.com>
2018-10-22 15:52:58 -0400

    add

--- namespace.js
+++ namespace.js
@@ -1,32 +1,32 @@
 var ZHOU0160 = {
-    init:function init() {
+    init: function () {
         let Div = document.createElement("div");
         Div.className = "box";
         Div.textContent = "zhou0160";
         document.getElementById("boxes").appendChild(Div);
 
-        Div.addEventListener("click", circleClick);
-        Div.addEventListener("mouseover", mouseOver);
-        Div.addEventListener("mouseout", mouseOut);
+        Div.addEventListener("click", ZHOU0160.circleClick);
+        Div.addEventListener("mouseover", ZHOU0160.mouseOver);
+        Div.addEventListener("mouseout", ZHOU0160.mouseOut);
+    },
+
+    circleClick: function () {
+        this.style.borderColor = "#53e6c2";
+        this.style.backgroundColor = "#ea9492";
+        console.log(this);
+    },
+
+    mouseOver: function (e) {
+        e.target.classList.toggle("highlight");
+        console.log(e);
+        console.log(e.target);
+    },
+
+    mouseOut: function (e) {
+        e.target.classList.toggle("highlight");
+        e.target.removeAttribute('style');
+        console.log(e);
+        console.log(e.target);
+
     }
-}
-
-function circleClick() {
-    this.style.borderColor = "#53e6c2";
-    this.style.backgroundColor = "#ea9492";
-    console.log(this);
-}
-
-function mouseOver(e) {
-    e.target.classList.toggle("highlight");
-    console.log(e);
-    console.log(e.target);
-}
-
-function mouseOut(e) {
-    e.target.classList.toggle("highlight");
-    e.target.removeAttribute('style');
-    console.log(e);
-    console.log(e.target);
-
 }
