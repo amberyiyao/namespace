f64d46df77fea87169b690b437eb43d815c31120
Amber Zhou <zhou0160@algonquinlive.com>
2018-10-24 23:14:22 -0400

    change

--- namespace.js
+++ namespace.js
@@ -5,22 +5,9 @@ var ZHOU0160 = {
         Div.textContent = "zhou0160";
         document.getElementById("boxes").appendChild(Div);
 
-<<<<<<< HEAD
         Div.addEventListener("click", cursorClick);
         Div.addEventListener("mouseover", mouseOver);
         Div.addEventListener("mouseout", mouseOut);
-
-=======
-        Div.addEventListener("click", ZHOU0160.cursorClick);
-        Div.addEventListener("mouseover", ZHOU0160.mouseOver);
-        Div.addEventListener("mouseout", ZHOU0160.mouseOut);
-    },
-
-    cursorClick: function () {
-        this.style.borderColor = "#53e6c2";
-        this.style.backgroundColor = "#ea9492";
-    },
->>>>>>> b04a5eee61c6e1de940137c8c09b858e877802a3
 
         function cursorClick() {
             this.style.borderColor = "#53e6c2";
