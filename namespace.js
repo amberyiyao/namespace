980117c269fbbd75d86d27f1a8c081978fc73812
zhou0160 <43050315+zhou0160@users.noreply.github.com>
2018-10-24 23:16:08 -0400

    Update namespace.js

--- namespace.js
+++ namespace.js
@@ -5,24 +5,22 @@ var ZHOU0160 = {
         Div.textContent = "zhou0160";
         document.getElementById("boxes").appendChild(Div);
 
-        Div.addEventListener("click", ZHOU0160.cursorClick);
-        Div.addEventListener("mouseover", ZHOU0160.mouseOver);
-        Div.addEventListener("mouseout", ZHOU0160.mouseOut);
-    },
+        Div.addEventListener("click", cursorClick);
+        Div.addEventListener("mouseover", mouseOver);
+        Div.addEventListener("mouseout", mouseOut);
 
-    cursorClick: function () {
-        this.style.borderColor = "#53e6c2";
-        this.style.backgroundColor = "#ea9492";
-    },
+        function cursorClick() {
+            this.style.borderColor = "#53e6c2";
+            this.style.backgroundColor = "#ea9492";
+        }
 
-    mouseOver: function (e) {
-        e.target.classList.toggle("highlight");
-    },
+        function mouseOver(e) {
+            e.target.classList.toggle("highlight");
+        }
 
-    mouseOut: function (e) {
-        e.target.classList.toggle("highlight");
-        e.target.removeAttribute('style');
+        function mouseOut(e) {
+            e.target.classList.toggle("highlight");
+            e.target.removeAttribute('style');
+        }
     }
 };
-
-  
