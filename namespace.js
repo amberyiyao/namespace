9571584e37d122e619d596dbd7df89a84fa518c2
Amber Zhou <zhou0160@algonquinlive.com>
2018-10-24 23:13:13 -0400

    Update namespace.js

--- namespace.js
+++ namespace.js
@@ -5,22 +5,23 @@ var ZHOU0160 = {
         Div.textContent = "zhou0160";
         document.getElementById("boxes").appendChild(Div);
 
-        Div.addEventListener("click", ZHOU0160.circleClick);
-        Div.addEventListener("mouseover", ZHOU0160.mouseOver);
-        Div.addEventListener("mouseout", ZHOU0160.mouseOut);
-    },
+        Div.addEventListener("click", cursorClick);
+        Div.addEventListener("mouseover", mouseOver);
+        Div.addEventListener("mouseout", mouseOut);
 
-    circleClick: function () {
-        this.style.borderColor = "#53e6c2";
-        this.style.backgroundColor = "#ea9492";
-    },
 
-    mouseOver: function (e) {
-        e.target.classList.toggle("highlight");
-    },
+        function cursorClick() {
+            this.style.borderColor = "#53e6c2";
+            this.style.backgroundColor = "#ea9492";
+        }
 
-    mouseOut: function (e) {
-        e.target.classList.toggle("highlight");
-        e.target.removeAttribute('style');
+        function mouseOver(e) {
+            e.target.classList.toggle("highlight");
+        }
+
+        function mouseOut(e) {
+            e.target.classList.toggle("highlight");
+            e.target.removeAttribute('style');
+        }
     }
 };
